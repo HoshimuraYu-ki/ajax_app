c3caf8867eb0ae931dfa56aab0da12984d1d17e3
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-02 14:32:43 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,11 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  # def new
-  # end
-
   def create
-    Post.create(content: params[:content])
-    redirrect_to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
+ 
 end
